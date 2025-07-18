2edf3067f720bcab9daac21398dae46ead572196
Girija <ramagirija95@gmail.com>
2025-07-18 18:03:09 +0530

    Time:50 ms (5.38%), Space:  17.9 MB (33.42%)

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res =''
+        n = min(len(word1), len(word2))
+        for i in range(n):
+            res = res + word1[i] + word2[i]
+        res = res + word1[n:] + word2[n:]
+        return res
