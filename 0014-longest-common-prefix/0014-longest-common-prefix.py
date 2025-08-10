4ea3b14f73d7c82c79058396e0210c38c020bad2
Girija <ramagirija95@gmail.com>
2025-08-10 16:28:18 +0530

    Time:3 ms (26.36%), Space:  18 MB (30.27%)

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,7 +4,7 @@ class Solution:
         res=""
         if s=="" or len(strs)==1:
             return s
-        for i in range(len(s)):
+        for i in range(len(s)+1):
             if all(st.startswith(s[:i]) for st in strs):
                 res=s[:i]
         return res
