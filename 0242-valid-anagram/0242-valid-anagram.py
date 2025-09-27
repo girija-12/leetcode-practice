d093ddf04f18eea581e76c987c4c41204be7a5b2
Girija <ramagirija95@gmail.com>
2025-09-27 23:06:49 +0530

    Time:11 ms (72.48%), Space:  18 MB (50.09%)

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+        if len(s)!=len(t):
+            return False
+        count=[0]*26
+        for ch1, ch2 in zip(s,t):
+            count[ord(ch1)-ord('a')]+=1
+            count[ord(ch2)-ord('a')]-=1
+        return all(c==0 for c in count)
