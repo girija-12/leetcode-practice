dcbfd268c8cffea656b8985711f45d079e8f6743
Girija <ramagirija95@gmail.com>
2025-11-19 08:39:29 +0530

    Time:1 ms (26.25%), Space:  18.1 MB (12.78%)

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        n=set(nums)
+        while original in n:
+            original*=2
+        return original
