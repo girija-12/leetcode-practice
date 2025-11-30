a0dcb3482afb265b09af62dc17a5de28db82f559
Girija <ramagirija95@gmail.com>
2025-11-30 20:11:25 +0530

    Time:16 ms (13.23%), Space:  12.7 MB (14.69%)

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,11 +5,6 @@
 #         self.next = next
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
         head=ListNode()
         t=head
         carry, sum=0,0
