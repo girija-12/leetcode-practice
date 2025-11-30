09f3aced5b0feea3ab00d35ecca01eaf257a3267
Girija <ramagirija95@gmail.com>
2025-11-30 20:06:38 +0530

    Time:4 ms (60.31%), Space:  19.1 MB (9.42%)

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,8 +1,5 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         x, size=0, len(nums)
         for i in range(size):
             if nums[i]!=0 and x<size:
