11d8d66879c586fd52c6aa9574f36198a94d45bb
Girija <ramagirija95@gmail.com>
2026-06-11 22:51:58 +0530

    Time:0 ms (100%), Space:  37.1 MB (99.47%)

--- 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.py
+++ 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.py
@@ -1,11 +1,3 @@
 class Solution:
     def findPrefixScore(self, nums: List[int]) -> List[int]:
-        pref_max=0
-        n=len(nums)
-        conver=[0]*n
-        for i in range(0,n):
-            pref_max=max(pref_max, nums[i])
-            conver[i]=nums[i]+pref_max
-        for i in range(1, n):
-            conver[i]=conver[i]+conver[i-1]
-        return conver
+        return (map(sum, zip(accumulate(nums), accumulate(accumulate(nums, max)))))
