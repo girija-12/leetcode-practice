9982e3f60c35200d181f3646f936df92dc2d1aa0
Girija <ramagirija95@gmail.com>
2026-06-21 19:33:23 +0530

    Time:99 ms (17.05%), Space:  32.2 MB (8.84%)

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        print(costs)
+        i=0
+        count=0
+        for x in costs:
+            i+=x
+            if i>coins:
+                break
+            count+=1
+        return count
