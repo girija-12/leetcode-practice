a04f8b8ba7718447184c69663f8392e077716a0d
Girija <ramagirija95@gmail.com>
2026-09-23 11:19:23 +0530

    Time:0 ms (100%), Space:  20.3 MB (59.51%)

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            try:
-                ind = nums.index(diff, i + 1)
-                return [i,ind]
-            except ValueError:
-                continue
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen={}
+        for i, val in enumerate(nums):
+            rem=target-val
+            if rem in seen:
+                return [i, seen[rem]]
+            seen[val]=i
